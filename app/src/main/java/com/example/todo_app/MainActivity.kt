c6763a11079c8a6cc428f030b98660740c73336f
Saeed Zare <44446617+MSaeed1381@users.noreply.github.com>
2022-08-09 18:38:10 +0300

    buttons and textViews Customized

--- app/src/main/java/com/example/todo_app/MainActivity.kt
+++ app/src/main/java/com/example/todo_app/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStore
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todo_app.data.TaskDataBase
 import com.example.todo_app.data.TaskRepository
